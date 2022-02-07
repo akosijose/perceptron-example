a950582778f9541702807fa2f28a717ff8a82719
akosijose <josegeraldlumbao0307@gmail.com>
2022-02-07 19:08:10 +0800

    imports

---
+++ perceptron.go
@@ -0,0 +1,6 @@
+// ## The code: A perceptron for classifying points
+
+// ### Imports
+
+// Besides a few standard libraries, we only need a small custom library for drawing the perceptron's output to a PNG.
+package main
